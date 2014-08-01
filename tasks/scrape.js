715419d60c63d1e2c320bb491017027180bd2649
Evan Krambuhl <thekrambuhl@gmail.com>
2014-08-01 02:41:20 -0700

    power laps!

--- tasks/scrape.js
+++ tasks/scrape.js
@@ -5,7 +5,7 @@ var $ = require('jquery');
 var _ = require('underscore');
 
 var starlaps = [];
-var powerlaps = {}
+var powerlaps = {};
 
 function listItems2collection(items) {
 	var collection = [];
@@ -20,7 +20,7 @@ function listItems2collection(items) {
 			time: time,
 			seconds: laptime2seconds(time),
 			note: '',
-			state: 'dry',
+			weather: 'dry',
 			passenger: false,
 			automatic: false,
 			url: 'http://en.wikipedia.org' + raw[1].attribs.href
@@ -30,7 +30,7 @@ function listItems2collection(items) {
 			var status = note2status(note.toLowerCase());
 			model.note = note;
 
-			if (status.state) { model.state = status.state; }
+			if (status.weather) { model.weather = status.weather; }
 			if (status.passenger) { model.passenger = status.passenger; }
 			if (status.automatic) { model.automatic = status.automatic; }
 		}
@@ -41,17 +41,19 @@ function listItems2collection(items) {
 	return collection;
 }
 
-function detectState(note) {
+function detectWeather(note) {
 	var keywords = [
 		'melted snow',
 		'very wet',
 		'mildly moist',
+		'partly damp',
 		'moist',
 		'wet',
 		'damp',
 		'ice',
 		'snow',
-		'hot'
+		'hot',
+		'cold'
 	];
 
 	var res;
@@ -75,18 +77,21 @@ function detectAutomatic(note) {
 
 function note2status(note) {
 	return {
-		state: detectState(note),
+		weather: detectWeather(note),
 		passenger: detectPassenger(note),
 		automatic: detectAutomatic(note)
 	};
 }
 
 function laptime2seconds(time) {
-	time = time.split(/[:.]/g).map(function (t) {
+	var chunks = time.split(/[:.]/g).map(function (t) {
 		return parseInt(t);
 	});
 
-	return (60 * time[0]) + (time[1]) + (time[2] / 10);
+	var secs = (60 * chunks[0]) + (chunks[1]) + (chunks[2] / 10);
+
+	if (_.isNaN(secs)) return time;
+	return secs;
 }
 
 function title2meta(title) {
@@ -101,31 +106,21 @@ function title2meta(title) {
 	};
 }
 
-function scrapeList(items, title) {
+function scrapeStarList(items, title) {
 	var meta = title2meta(title);
 	return {
-		make: meta.make,
-		model: meta.model,
+		vehicle: meta.make + ' ' + meta.model,
 		start: meta.start,
 		end: meta.end,
 		data: listItems2collection(items)
 	};
 }
 
-function scrapeData(el) {
+function scrapeStarData(el) {
 	var root = el.parent();
 	var listItems = root.next().find('li');
 	var title = root.prevAll('h4').eq(0).children()[0].children[0].data;
-	return scrapeList(listItems, title);
-}
-
-var loaded = 0;
-function queueWrite() {
-	loaded++
-
-	if (loaded >= 2) {
-		fs.writeFile('data/top-gear.json', JSON.stringify(_.union(starlaps, [powerlaps]), null, 2));
-	}
+	return scrapeStarList(listItems, title);
 }
 
 var starUrl = 'http://en.wikipedia.org/wiki/Top_Gear_test_track';
@@ -136,27 +131,81 @@ request(starUrl, function(error, response, html){
 		$('.mw-headline').filter(function () {
 			return this.attribs.id.indexOf('leaderboard') !== -1;
 		}).map(function (list) {
-			var data = scrapeData($(this));
+			var data = scrapeStarData($(this));
 			starlaps.push(data);
 		});
 
 		fs.writeFile('data/starlaps.json', JSON.stringify(starlaps, null, 2));
-		queueWrite();
 	}
 });
-//
-// var powerUrl = 'http://en.wikipedia.org/wiki/Top_Gear_test_track';
-// request(powerUrl, function(error, response, html){
-// 	if(!error){
-// 		var $ = cheerio.load(html);
-//
-// 		$('.mw-headline').filter(function () {
-// 			return this.attribs.id.indexOf('leaderboard') !== -1;
-// 		}).map(function (list) {
-// 			powerlaps = scrapeData($(this));
-// 		});
-//
-// 		fs.writeFile('data/powerlaps.json', JSON.stringify(powerlaps, null, 2));
-// 		queueWrite();
-// 	}
-// });
+
+
+
+function getVehicle(nodes) {
+	var data = [];
+
+	nodes.forEach(function (node, i) {
+		if (node.type == 'text') {
+			data.push(node.data);
+		} else if (node.type == 'tag') {
+			data.push(node.children[0].data);
+		}
+	});
+
+	var join = data.join('');
+	var hasNotes = join.indexOf('(') !== -1;
+
+	var ret = {
+		string: join.toLowerCase(),
+		vehicle: join.split('(')[0].trim(),
+	};
+
+	if (hasNotes) {
+		ret.notes = join.substr(join.indexOf('(')).replace(/[()]/g,'').trim();
+	}
+
+	return ret;
+}
+
+function scrapePowerData(table) {
+	var rows = table.children('tr');
+	var laps = [];
+
+	rows.each(function (i) {
+		var power;
+		var cells = rows.eq(i);
+
+		if(cells.children()[1].name !== 'th') {
+			var rawdata = getVehicle(cells.children()[1].children);
+			var time = cells.children()[0].children[0].data;
+
+			power = {
+				position: i - 1,
+				time: time,
+				seconds: laptime2seconds(time),
+				vehicle: rawdata.vehicle
+			};
+
+			if (rawdata.notes) {
+				power.weather = detectWeather(rawdata.notes);
+				if (_.isUndefined(power.weather)) delete power.weather;
+
+			}
+
+			laps.push(power);
+		}
+	});
+
+	return laps;
+}
+
+
+var powerUrl = 'http://en.wikipedia.org/wiki/List_of_Top_Gear_test_track_Power_Lap_Times';
+request(powerUrl, function(error, response, html){
+	if(!error){
+		var $ = cheerio.load(html);
+		var table = $('#Qualifying_vehicles').parent().nextAll('table').eq(0);
+
+		fs.writeFile('data/powerlaps.json', JSON.stringify(scrapePowerData(table), null, 2));
+	}
+});
